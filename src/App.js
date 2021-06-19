da94be3794aacf688b48c8ae4ab3af2a2887abf8
Lario <osunkunleolanrewaju@gmail.com>
2021-06-19 16:09:11 +0100

    started the footer section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FooterContainer } from './containers/FooterContainer'
 import JumbotronContainer from './containers/JumbotronContainer'
 
 export default function App() {
   return (
+    <>
     <JumbotronContainer />
+    <FooterContainer />
+    </>
   )
 }
