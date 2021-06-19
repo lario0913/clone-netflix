686e135724ce2115f675d00c14812f03be6586a6
Lario <osunkunleolanrewaju@gmail.com>
2021-06-19 19:36:21 +0100

    completed the footer component

--- src/containers/FooterContainer.js
+++ src/containers/FooterContainer.js
@@ -5,6 +5,39 @@ export function FooterContainer(){
     return (
         <Footer>
             <Footer.Title>Questions? Contact us.</Footer.Title>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href="">FAQ</Footer.Link>
+                    <Footer.Link href="">Investor Relations</Footer.Link>
+                    <Footer.Link href="">Ways to Watch</Footer.Link>
+                    <Footer.Link href="">Cooperate Information</Footer.Link>
+                    <Footer.Link href="">Netflix Original</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="">Help Centre</Footer.Link>
+                    <Footer.Link href="">Jobs</Footer.Link>
+                    <Footer.Link href="">Terms of Use </Footer.Link>
+                    <Footer.Link href="">Contact Us</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="">Account</Footer.Link>
+                    <Footer.Link href="">Redeem Gift Cards</Footer.Link>
+                    <Footer.Link href="">Privacy </Footer.Link>
+                    <Footer.Link href="">Speed Test</Footer.Link>
+                </Footer.Column>
+            
+                <Footer.Column>
+                    <Footer.Link href="">Media Centre</Footer.Link>
+                    <Footer.Link href="">Buy Gift Cards</Footer.Link>
+                    <Footer.Link href="">Cookie Prefences </Footer.Link>
+                    <Footer.Link href="">Legal Notices</Footer.Link>
+                </Footer.Column>
+            
+                <Footer.Break />
+                <Footer.Text>Netflix United Kingdom</Footer.Text>
+            </Footer.Row>
         </Footer>
     )
 }
