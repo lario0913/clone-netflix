6514b9fe82294745987c5b65c74f6131efeaf454
Lario <osunkunleolanrewaju@gmail.com>
2021-06-29 22:33:32 +0100

    getting started with the header component

---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Header} from '../components'
+import * as ROUTES from '../constants/routes'
+
+export default function HeaderContainer({children}){
+    return(
+        <Header>
+            <Header.Frame>
+                <Header.Logo to={ROUTES.HOME} alt="Netflix" />
+                <Header.ButtonLink to={ROUTES.SIGN_IN}>SIgn In</Header.ButtonLink>
+            </Header.Frame>
+            {children}
+        </Header>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { FaqsContainer } from '../containers/FaqsContainer'
 import { FooterContainer } from '../containers/FooterContainer'
+import HeaderContainer from '../containers/HeaderContainer'
 import JumbotronContainer from '../containers/JumbotronContainer'
 
 export default function Home(){
     return (
         <>
+            <HeaderContainer />
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
