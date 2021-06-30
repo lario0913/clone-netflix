362a3fb286f804c40199278e54923cce37df3faa
Lario <osunkunleolanrewaju@gmail.com>
2021-06-30 15:45:16 +0100

    fix header error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaqsContainer } from '../containers/FaqsContainer'
 import { FooterContainer } from '../containers/FooterContainer'
-import { OptForm } from '../components'
+import { OptForm, Feature } from '../components'
 import HeaderContainer from '../containers/HeaderContainer'
 import JumbotronContainer from '../containers/JumbotronContainer'
 
@@ -9,12 +9,16 @@ export default function Home(){
     return (
         <>
             <HeaderContainer>
-            <OptForm>
-               <OptForm.Input placeholder="Email address" /> 
-               <OptForm.Button>Try it now</OptForm.Button>
-               <OptForm.Break />
-               <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
-            </OptForm>
+                <Feature>
+                    <Feature.Title>Unlimited Films, TV programmes and more</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Input placeholder="Email address" /> 
+                        <OptForm.Button>Try it now</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
